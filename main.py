7bb25f674ca62027db033cf8abe984b07c1ba083
rocky <dineshofficialkd@gmail.com>
2026-09-01 21:18:32 +0530

    complted first langgraph

--- main.py
+++ main.py
@@ -6,41 +6,42 @@ from langgraph.graph import MessagesState, StateGraph, START, END
 
 from nodes import run_agent_reasoning_engine,tool_node
 
-AGENT_REASON = "agent_reason"
-ACT ="act"
-LAST=-1
+AGENT_BRAIN = "agent_brain"
+WORKER = "worker"
+LAST =-1
 
-def should_continue(state: dict) ->str:
+flow = StateGraph(MessagesState)
+
+flow.add_node(AGENT_BRAIN,run_agent_reasoning_engine)
+flow.set_entry_point(AGENT_BRAIN)
+flow.add_node(WORKER,tool_node)
+
+def should_continue(state:dict) -> str:
     if not state["messages"][LAST].tool_calls:
-        return END
-    return ACT
-
-flow= StateGraph(MessagesState )
-flow.add_node(AGENT_REASON,run_agent_reasoning_engine)
-flow.set_entry_point(AGENT_REASON)
-flow.add_node(ACT,tool_node)
-
+       return END
+    return WORKER
 flow.add_conditional_edges(
-    AGENT_REASON, should_continue,
+    AGENT_BRAIN,
+    should_continue,
     {
         END:END,
-        ACT:ACT
-    }
+        WORKER:WORKER,
+    },
 )
-flow.add_edge(ACT,AGENT_REASON)
-app=flow.compile()
-app.get_graph().draw_mermaid_png(output_file_path="flow.png")
+flow.add_edge(WORKER,AGENT_BRAIN)
+
+app= flow.compile()
+app.get_graph().draw_mermaid_png(output_file_path="flown.png")
+
 
 if __name__=="__main__":
-    print("Hello ReAct with LangGraph")
+    print("react langgraph agent")
 
-    res = app.invoke(
+    res= app.invoke(
         {
-            "messages":[
-                HumanMessage(
-                    content="what is the weather in sf? List it and then Triple it "
-                )
-            ]
+        "messages":[
+            HumanMessage(content="what is the weather in chennai? List it and then trible it ")
+        ]
         }
     )
     print(res["messages"][LAST].content)
